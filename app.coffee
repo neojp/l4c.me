16223a196ab26be57ffe5b0713947b5147331c86
Joan Piedra <theneojp@gmail.com>
2012-02-19 16:12:55 -0500

    New URLS for templates, authentication with Passport, HVMC and helpers

--- app.coffee
+++ app.coffee
@@ -2,6 +2,48 @@
 express = require('express')
 _ = underscore = require('underscore')
 app = module.exports = express.createServer()
+passport = require('passport')
+LocalStrategy = require('passport-local').Strategy
+
+# Passport configuration
+users = [
+		id: 1337
+		username: 'neojp',
+		password: 'password'
+	,
+		id: 666
+		username: 'freddier',
+		password: 'password'
+	,
+		id: 161803399
+		username: 'maikel',
+		password: 'password'
+	,
+		id: 123
+		username: 'leonidas',
+		password: 'password'
+]
+
+
+passport.serializeUser (user, next) ->
+	next null, user.id
+
+
+passport.deserializeUser (id, next) ->
+	user = _.filter users, (i) -> i.id == id
+	if _.size user
+		next null, _.first user
+	else
+		next null, false
+
+
+passport.use new LocalStrategy (username, password, next) ->
+	process.nextTick () ->
+		user = _.filter users, (i) -> i.username == username && i.password == password
+		if _.size user
+			next null, _.first user
+		else
+			next null, false
 
 
 # Configuration
@@ -10,16 +52,37 @@ app.configure ->
 	app.set 'view engine', 'jade'
 	app.set 'strict routing', true
 
-	# app.use express.favicon()
-	app.use express.bodyParser()
-	app.use express.logger( format: ':status ":method :url"' )
-	
 	oneYear = 31556926000; # 1 year on milliseconds
 	app.use express.static( __dirname + '/public', maxAge: oneYear )
+	app.use express.logger( format: ':status ":method :url"' )
+
+	# app.use express.favicon()
+
+	app.use express.cookieParser()
+	app.use express.bodyParser()
+	app.use express.methodOverride()
+	app.use express.session secret: '♥'
+
+	app.use passport.initialize()
+	app.use passport.session()
+	
+	app.use app.router
 
 
 # Middleware
 middleware =
+	# auth: (path = '/') -> (req, res, next) ->
+	# 	passport.authenticate 'local', sucessRedirect: path, failureRedirect: '/login'
+	
+	auth: (req, res, next) ->
+		if req.isAuthenticated()
+			return next()
+		
+		# console.log req.originalUrl
+		req.flash 'auth_redirect', req.originalUrl
+		res.redirect('/login')
+	
+
 	hmvc: (path) -> (req, res, next) ->
 		route = app.match.get(path)
 		route = _.filter route, (i) -> i.path == path
@@ -40,6 +103,7 @@ middleware =
 			redirection = redirection.replace i, req.params[ i.substring(1) ]
 		
 		page = parseInt req.param 'page', 1
+		res.local 'page', page
 		
 		if page == 1
 			return res.redirect redirection, 301
@@ -56,6 +120,32 @@ middleware =
 			return res.redirect url, 301
 		
 		next()
+
+
+# Helpers
+helpers =
+	slugify: (str) ->
+		str = str.replace /^\s+|\s+$/g, ''
+		str = str.toLowerCase()
+
+		# remove accents, swap ñ for n, etc
+		from = "àáäâèéëêìíïîòóöôùúüûñç®©·/_,:;"
+		to   = "aaaaeeeeiiiioooouuuuncrc------"
+
+		for i, character of from.split ''
+			str = str.replace new RegExp(character, 'g'), to.charAt i
+		
+		# trademark sign
+		str = str.replace new RegExp('™', 'g'), 'tm'
+
+		# remove invalid chars
+		str = str.replace /[^a-z0-9 -]/g, ''
+
+		# collapse whitespace and replace by -
+		str = str.replace /\s+/g, '-'
+		
+		# collapse dashes
+		str = str.replace /-+/g, '-'
 
 
 # Route Params
@@ -89,7 +179,7 @@ app.param 'sort', (req, res, next, id) ->
 
 
 app.param 'user', (req, res, next, id) ->
-	if id not in ['ultimas', 'top', 'galeria', 'pag']
+	if id not in ['ultimas', 'top', 'galeria', 'pag', 'publicar']
 		next()
 	else
 		return next('route')
@@ -100,13 +190,15 @@ app.all '*', middleware.remove_trailing_slash, (req, res, next) ->
 	res.locals
 		_: underscore
 		body_class: ''
+		user: if req.isAuthenticated() then req.user else null
+		page: parseInt req.param 'page', 1
 	
+	res.locals helpers
+
 	next('route')
 
 
-app.get '/', (req, res) ->
-	res.local 'body_class', 'home gallery'
-	res.render 'gallery'
+app.get '/', middleware.hmvc('/fotos')
 
 
 app.get '/fotos/:user/:slug', (req, res) ->
@@ -114,19 +206,19 @@ app.get '/fotos/:user/:slug', (req, res) ->
 	res.render 'gallery_single'
 
 
-app.get '/fotos/:user/:slug/editar', (req, res) ->
+app.get '/fotos/:user/:slug/editar', middleware.auth, (req, res) ->
 	user = req.param 'user'
 	slug = req.param 'slug'
 	res.send "PUT /fotos/#{user}/#{slug}/editar", 'Content-Type': 'text/plain'
 
 
-app.put '/fotos/:user/:slug', (req, res) ->
+app.put '/fotos/:user/:slug', middleware.auth, (req, res) ->
 	user = req.param 'user'
 	slug = req.param 'slug'
 	res.send "PUT /fotos/#{user}/#{slug}", 'Content-Type': 'text/plain'
 
 
-app.delete '/fotos/:user/:slug', (req, res) ->
+app.delete '/fotos/:user/:slug', middleware.auth, (req, res) ->
 	user = req.param 'user'
 	slug = req.param 'slug'
 	res.send "DELETE /fotos/#{user}/#{slug}", 'Content-Type': 'text/plain'
@@ -142,27 +234,31 @@ app.get '/fotos/:user', (req, res) ->
 	res.render 'gallery'
 
 
-app.get '/fotos/publicar', (req, res) ->
+app.get '/fotos/publicar', middleware.auth, (req, res) ->
 	res.render 'gallery_upload'
 
 
-app.post '/fotos/publicar', (req, res) ->
+app.post '/fotos/publicar', middleware.auth, (req, res) ->
 	res.send "POST /fotos/publicar", 'Content-Type': 'text/plain'
 
 
 app.get '/fotos/:sort/pag/:page?', middleware.paged('/fotos/:sort')
 app.get '/fotos/:sort', (req, res, next) ->
 	sort = req.params.sort
-	page = parseInt req.param 'page', 1
-
-	res.send "GET /fotos/#{sort}/pag/#{page}", 'Content-Type': 'text/plain'
+	res.locals
+		sort: sort
+		path: "/fotos/#{sort}"
+		body_class: "gallery liquid #{sort}"
+	
+	res.render 'gallery'
 
 
 app.get '/fotos/pag/:page?', middleware.paged('/fotos')
 app.get '/fotos', (req, res) ->
-	res.send "GET /fotos", 'Content-Type': 'text/plain'
-	# res.local 'body_class', 'gallery liquid'
-	# res.render 'gallery'
+	res.local 'path', '/fotos'
+	# res.send "GET /fotos", 'Content-Type': 'text/plain'
+	res.local 'body_class', 'gallery liquid'
+	res.render 'gallery'
 
 
 app.get '/tags/:tag/pag/:page?', middleware.paged('/tags/:tag')
@@ -174,23 +270,35 @@ app.get '/tags/:tag', (req, res) ->
 
 
 app.get '/tags', (req, res) ->
-	res.send "GET /fotos/tags", 'Content-Type': 'text/plain'
+	res.send "GET /tags", 'Content-Type': 'text/plain'
 
 
-app.get '/perfil', (req, res) ->
+app.get '/perfil', middleware.auth, (req, res) ->
 	res.send "GET /perfil", 'Content-Type': 'text/plain'
 
 
-app.put '/perfil', (req, res) ->
+app.put '/perfil', middleware.auth, (req, res) ->
 	res.send "PUT /perfil", 'Content-Type': 'text/plain'
 
 
 app.get '/login', (req, res) ->
-	res.send "GET /login", 'Content-Type': 'text/plain'
+	if (req.isAuthenticated())
+		return res.redirect '/'
+	
+	res.render 'login'
+	# res.send "GET /login", 'Content-Type': 'text/plain'
 
 
-app.post '/login', (req, res) ->
-	res.send "POST /login", 'Content-Type': 'text/plain'
+app.post '/login', passport.authenticate('local', failureRedirect: '/login'), (req, res) ->
+	flash = req.flash('auth_redirect')
+	url = if _.size flash then _.first flash else '/'
+	res.redirect url
+
+
+app.get '/logout', (req, res) ->
+	req.logout()
+	res.redirect '/'
+	# res.send "GET /perfil", 'Content-Type': 'text/plain'
 
 
 app.get '/registro', (req, res) ->
@@ -201,11 +309,7 @@ app.post '/registro', (req, res) ->
 	res.send "POST /registro", 'Content-Type': 'text/plain'
 
 
-app.get '/logout', (req, res) ->
-	res.send "GET /perfil", 'Content-Type': 'text/plain'
-
-
-app.get '/tweets', (req, res) ->
+app.get '/tweets', middleware.auth, (req, res) ->
 	res.send "GET /tweets", 'Content-Type': 'text/plain'
 
 
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, express, middleware, underscore, _;
+  var LocalStrategy, app, express, helpers, middleware, passport, underscore, users, _;
 
   express = require('express');
 
@@ -7,22 +7,89 @@
 
   app = module.exports = express.createServer();
 
+  passport = require('passport');
+
+  LocalStrategy = require('passport-local').Strategy;
+
+  users = [
+    {
+      id: 1337,
+      username: 'neojp',
+      password: 'password'
+    }, {
+      id: 666,
+      username: 'freddier',
+      password: 'password'
+    }, {
+      id: 161803399,
+      username: 'maikel',
+      password: 'password'
+    }, {
+      id: 123,
+      username: 'leonidas',
+      password: 'password'
+    }
+  ];
+
+  passport.serializeUser(function(user, next) {
+    return next(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, next) {
+    var user;
+    user = _.filter(users, function(i) {
+      return i.id === id;
+    });
+    if (_.size(user)) {
+      return next(null, _.first(user));
+    } else {
+      return next(null, false);
+    }
+  });
+
+  passport.use(new LocalStrategy(function(username, password, next) {
+    return process.nextTick(function() {
+      var user;
+      user = _.filter(users, function(i) {
+        return i.username === username && i.password === password;
+      });
+      if (_.size(user)) {
+        return next(null, _.first(user));
+      } else {
+        return next(null, false);
+      }
+    });
+  }));
+
   app.configure(function() {
     var oneYear;
     app.set('views', __dirname + '/public/templates');
     app.set('view engine', 'jade');
     app.set('strict routing', true);
-    app.use(express.bodyParser());
+    oneYear = 31556926000;
+    app.use(express.static(__dirname + '/public', {
+      maxAge: oneYear
+    }));
     app.use(express.logger({
       format: ':status ":method :url"'
     }));
-    oneYear = 31556926000;
-    return app.use(express.static(__dirname + '/public', {
-      maxAge: oneYear
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.session({
+      secret: '♥'
     }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    return app.use(app.router);
   });
 
   middleware = {
+    auth: function(req, res, next) {
+      if (req.isAuthenticated()) return next();
+      req.flash('auth_redirect', req.originalUrl);
+      return res.redirect('/login');
+    },
     hmvc: function(path) {
       return function(req, res, next) {
         var callback, route;
@@ -51,6 +118,7 @@
           redirection = redirection.replace(i, req.params[i.substring(1)]);
         }
         page = parseInt(req.param('page', 1));
+        res.local('page', page);
         if (page === 1) return res.redirect(redirection, 301);
         return middleware.hmvc(path)(req, res, next);
       };
@@ -64,6 +132,25 @@
         return res.redirect(url, 301);
       }
       return next();
+    }
+  };
+
+  helpers = {
+    slugify: function(str) {
+      var character, from, i, to, _ref;
+      str = str.replace(/^\s+|\s+$/g, '');
+      str = str.toLowerCase();
+      from = "àáäâèéëêìíïîòóöôùúüûñç®©·/_,:;";
+      to = "aaaaeeeeiiiioooouuuuncrc------";
+      _ref = from.split('');
+      for (i in _ref) {
+        character = _ref[i];
+        str = str.replace(new RegExp(character, 'g'), to.charAt(i));
+      }
+      str = str.replace(new RegExp('™', 'g'), 'tm');
+      str = str.replace(/[^a-z0-9 -]/g, '');
+      str = str.replace(/\s+/g, '-');
+      return str = str.replace(/-+/g, '-');
     }
   };
 
@@ -101,7 +188,7 @@
   });
 
   app.param('user', function(req, res, next, id) {
-    if (id !== 'ultimas' && id !== 'top' && id !== 'galeria' && id !== 'pag') {
+    if (id !== 'ultimas' && id !== 'top' && id !== 'galeria' && id !== 'pag' && id !== 'publicar') {
       return next();
     } else {
       return next('route');
@@ -111,22 +198,22 @@
   app.all('*', middleware.remove_trailing_slash, function(req, res, next) {
     res.locals({
       _: underscore,
-      body_class: ''
+      body_class: '',
+      user: req.isAuthenticated() ? req.user : null,
+      page: parseInt(req.param('page', 1))
     });
+    res.locals(helpers);
     return next('route');
   });
 
-  app.get('/', function(req, res) {
-    res.local('body_class', 'home gallery');
-    return res.render('gallery');
-  });
+  app.get('/', middleware.hmvc('/fotos'));
 
   app.get('/fotos/:user/:slug', function(req, res) {
     res.local('body_class', 'single');
     return res.render('gallery_single');
   });
 
-  app.get('/fotos/:user/:slug/editar', function(req, res) {
+  app.get('/fotos/:user/:slug/editar', middleware.auth, function(req, res) {
     var slug, user;
     user = req.param('user');
     slug = req.param('slug');
@@ -135,7 +222,7 @@
     });
   });
 
-  app.put('/fotos/:user/:slug', function(req, res) {
+  app.put('/fotos/:user/:slug', middleware.auth, function(req, res) {
     var slug, user;
     user = req.param('user');
     slug = req.param('slug');
@@ -144,7 +231,7 @@
     });
   });
 
-  app["delete"]('/fotos/:user/:slug', function(req, res) {
+  app["delete"]('/fotos/:user/:slug', middleware.auth, function(req, res) {
     var slug, user;
     user = req.param('user');
     slug = req.param('slug');
@@ -163,11 +250,11 @@
     return res.render('gallery');
   });
 
-  app.get('/fotos/publicar', function(req, res) {
+  app.get('/fotos/publicar', middleware.auth, function(req, res) {
     return res.render('gallery_upload');
   });
 
-  app.post('/fotos/publicar', function(req, res) {
+  app.post('/fotos/publicar', middleware.auth, function(req, res) {
     return res.send("POST /fotos/publicar", {
       'Content-Type': 'text/plain'
     });
@@ -176,20 +263,22 @@
   app.get('/fotos/:sort/pag/:page?', middleware.paged('/fotos/:sort'));
 
   app.get('/fotos/:sort', function(req, res, next) {
-    var page, sort;
+    var sort;
     sort = req.params.sort;
-    page = parseInt(req.param('page', 1));
-    return res.send("GET /fotos/" + sort + "/pag/" + page, {
-      'Content-Type': 'text/plain'
+    res.locals({
+      sort: sort,
+      path: "/fotos/" + sort,
+      body_class: "gallery liquid " + sort
     });
+    return res.render('gallery');
   });
 
   app.get('/fotos/pag/:page?', middleware.paged('/fotos'));
 
   app.get('/fotos', function(req, res) {
-    return res.send("GET /fotos", {
-      'Content-Type': 'text/plain'
-    });
+    res.local('path', '/fotos');
+    res.local('body_class', 'gallery liquid');
+    return res.render('gallery');
   });
 
   app.get('/tags/:tag/pag/:page?', middleware.paged('/tags/:tag'));
@@ -204,33 +293,40 @@
   });
 
   app.get('/tags', function(req, res) {
-    return res.send("GET /fotos/tags", {
+    return res.send("GET /tags", {
       'Content-Type': 'text/plain'
     });
   });
 
-  app.get('/perfil', function(req, res) {
+  app.get('/perfil', middleware.auth, function(req, res) {
     return res.send("GET /perfil", {
       'Content-Type': 'text/plain'
     });
   });
 
-  app.put('/perfil', function(req, res) {
+  app.put('/perfil', middleware.auth, function(req, res) {
     return res.send("PUT /perfil", {
       'Content-Type': 'text/plain'
     });
   });
 
   app.get('/login', function(req, res) {
-    return res.send("GET /login", {
-      'Content-Type': 'text/plain'
-    });
+    if (req.isAuthenticated()) return res.redirect('/');
+    return res.render('login');
   });
 
-  app.post('/login', function(req, res) {
-    return res.send("POST /login", {
-      'Content-Type': 'text/plain'
-    });
+  app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/login'
+  }), function(req, res) {
+    var flash, url;
+    flash = req.flash('auth_redirect');
+    url = _.size(flash) ? _.first(flash) : '/';
+    return res.redirect(url);
+  });
+
+  app.get('/logout', function(req, res) {
+    req.logout();
+    return res.redirect('/');
   });
 
   app.get('/registro', function(req, res) {
@@ -245,13 +341,7 @@
     });
   });
 
-  app.get('/logout', function(req, res) {
-    return res.send("GET /perfil", {
-      'Content-Type': 'text/plain'
-    });
-  });
-
-  app.get('/tweets', function(req, res) {
+  app.get('/tweets', middleware.auth, function(req, res) {
     return res.send("GET /tweets", {
       'Content-Type': 'text/plain'
     });
