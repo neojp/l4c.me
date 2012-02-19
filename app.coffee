70efd4f23f1b4df99836a48c4f6893f9a12482d1
Joan Piedra <theneojp@gmail.com>
2012-02-19 05:02:11 -0500

    Moving to Coffee, creating all routes available

---
+++ app.coffee
@@ -0,0 +1,202 @@
+# Module dependencies
+express = require('express')
+_ = underscore = require('underscore')
+app = module.exports = express.createServer()
+
+
+# Configuration
+app.configure ->
+	app.set 'views', __dirname + '/public/templates'
+	app.set 'view engine', 'jade'
+	app.set 'strict routing', true
+
+	app.use express.favicon()
+	app.use express.bodyParser()
+	app.use express.logger( format: ':status ":method :url"' )
+	
+	oneYear = 31556926000; # 1 year on milliseconds
+	app.use express.static( __dirname + '/public', maxAge: oneYear )
+
+
+# Middleware
+middleware =
+	sort: (req, res, next) ->
+		next('route') if not _.include ['ultimas', 'top', 'galeria'], req.params.sort
+	
+
+	hmvc: (req, res, next, path) ->
+		route = app.match.get(path)
+		route = _.filter route, (i) -> i.path == path
+		route = _.first route
+		callback = _.last route.callbacks
+		
+		if _.isFunction callback
+			callback(req, res)
+		else
+			next('route')
+	
+
+	paged: (path) -> (req, res, next) ->
+		redirection = path + ''
+		path_vars = _.filter path.split('/'), (i) -> i.charAt(0) == ':'
+
+		for path_var, i of path_vars
+			redirection = redirection.replace i, req.params[ i.substring(1) ]
+		
+		page = parseInt req.param 'page', 1
+		
+		if page == 1
+			return res.redirect redirection, 301
+		
+		middleware.hmvc(req, res, next, path)
+	
+
+	remove_trailing_slash: (req, res, next) ->
+		url = req.originalUrl
+		
+		if url.length > 1 && url.charAt(url.length - 1) == '/'
+			url = url.substring 0, url.length - 1
+			return res.redirect url, 301
+		
+		next()
+
+
+# Route Params
+app.param 'sort', (req, res, next, id) ->
+	if id in ['ultimas', 'top', 'galeria']
+		next()
+	else
+		return next('route')
+
+app.param 'user', (req, res, next, id) ->
+	if id not in ['ultimas', 'top', 'galeria', 'pag']
+		next()
+	else
+		return next('route')
+
+app.param 'page', (req, res, next, id) ->
+	if id.match /[0-9]+/
+		req.param.page = parseInt req.param.page
+		next()
+	else
+		return next(404)
+
+
+# Routes
+app.all '*', middleware.remove_trailing_slash, (req, res, next) ->
+	res.locals
+		_: underscore
+		body_class: ''
+	
+	next('route')
+
+
+app.get '/', (req, res) ->
+	res.local 'body_class', 'home gallery'
+	res.render 'gallery'
+
+
+app.get '/fotos/:user/:slug', (req, res) ->
+	res.local 'body_class', 'single'
+	res.render 'gallery_single'
+
+
+app.get '/fotos/:user/:slug/editar', (req, res) ->
+	user = req.param 'user'
+	slug = req.param 'slug'
+	res.send "PUT /fotos/#{user}/#{slug}/editar", 'Content-Type': 'text/plain'
+
+
+app.put '/fotos/:user/:slug', (req, res) ->
+	user = req.param 'user'
+	slug = req.param 'slug'
+	res.send "PUT /fotos/#{user}/#{slug}", 'Content-Type': 'text/plain'
+
+
+app.delete '/fotos/:user/:slug', (req, res) ->
+	user = req.param 'user'
+	slug = req.param 'slug'
+	res.send "DELETE /fotos/#{user}/#{slug}", 'Content-Type': 'text/plain'
+
+
+app.get '/fotos/:user/:slug/sizes/:size', (req, res) ->
+	res.local 'body_class', 'sizes'
+	res.render 'gallery_single_large'
+
+
+app.get '/fotos/:user', (req, res) ->
+	res.local 'body_class', 'gallery liquid'
+	res.render 'gallery'
+
+
+app.get '/fotos/publicar', (req, res) ->
+	res.render 'gallery_upload'
+
+
+app.post '/fotos/publicar', (req, res) ->
+	res.send "POST /fotos/publicar", 'Content-Type': 'text/plain'
+
+
+app.get '/fotos/:sort/pag/:page?', middleware.paged('/fotos/:sort')
+app.get '/fotos/:sort', (req, res, next) ->
+	sort = req.params.sort
+	page = parseInt req.param 'page', 1
+
+	res.send "GET /fotos/#{sort}/pag/#{page}", 'Content-Type': 'text/plain'
+
+
+app.get '/fotos/pag/:page?', middleware.paged('/fotos')
+app.get '/fotos', (req, res) ->
+	res.send "GET /fotos", 'Content-Type': 'text/plain'
+	# res.local 'body_class', 'gallery liquid'
+	# res.render 'gallery'
+
+
+app.get '/tags/:tag/pag/:page?', middleware.paged('/tags/:tag')
+app.get '/tags/:tag', (req, res) ->
+	tag = req.params.tag
+	page = parseInt req.param 'page', 1
+
+	res.send "GET /tags/#{tag}/pag/#{page}", 'Content-Type': 'text/plain'
+
+
+app.get '/tags', (req, res) ->
+	res.send "GET /fotos/tags", 'Content-Type': 'text/plain'
+
+
+app.get '/perfil', (req, res) ->
+	res.send "GET /perfil", 'Content-Type': 'text/plain'
+
+
+app.put '/perfil', (req, res) ->
+	res.send "PUT /perfil", 'Content-Type': 'text/plain'
+
+
+app.get '/login', (req, res) ->
+	res.send "GET /login", 'Content-Type': 'text/plain'
+
+
+app.post '/login', (req, res) ->
+	res.send "POST /login", 'Content-Type': 'text/plain'
+
+
+app.get '/registro', (req, res) ->
+	res.send "GET /registro", 'Content-Type': 'text/plain'
+
+
+app.post '/registro', (req, res) ->
+	res.send "POST /registro", 'Content-Type': 'text/plain'
+
+
+app.get '/logout', (req, res) ->
+	res.send "GET /perfil", 'Content-Type': 'text/plain'
+
+
+# app.all '*', (req, res) ->
+# 	res.render 'gallery'
+
+
+# Only listen on $ node app.js
+if (!module.parent)
+	app.listen 3000
+	console.log "Listening on port %d", app.address().port
--- app.js
+++ app.js
@@ -1,48 +1,246 @@
-// Module dependencies
-var express = require('express'),
-    _ = underscore = require('underscore'),
-    sys = require('sys'),
-    app     = module.exports = express.createServer(),
-    options = { locals: { _: underscore, sys: sys, body_class: '' } };
+(function() {
+  var app, express, middleware, underscore, _;
 
+  express = require('express');
 
-// Configuration
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  _ = underscore = require('underscore');
 
-  app.use(express.favicon());
-  app.use(express.bodyParser());
-  //app.use(express.logger({ format: ':status ":method :url"' }));
-  
-  var oneYear = 31556926000; // 1 year on milliseconds
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-});
+  app = module.exports = express.createServer();
 
+  app.configure(function() {
+    var oneYear;
+    app.set('views', __dirname + '/public/templates');
+    app.set('view engine', 'jade');
+    app.set('strict routing', true);
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.logger({
+      format: ':status ":method :url"'
+    }));
+    oneYear = 31556926000;
+    return app.use(express.static(__dirname + '/public', {
+      maxAge: oneYear
+    }));
+  });
 
-// Routes
-app.get('/fullsize/', function(req, res){
-  options.locals.body_class = 'sizes';
-  res.render('gallery_single_large', options);
-});
+  middleware = {
+    sort: function(req, res, next) {
+      if (!_.include(['ultimas', 'top', 'galeria'], req.params.sort)) {
+        return next('route');
+      }
+    },
+    hmvc: function(req, res, next, path) {
+      var callback, route;
+      route = app.match.get(path);
+      route = _.filter(route, function(i) {
+        return i.path === path;
+      });
+      route = _.first(route);
+      callback = _.last(route.callbacks);
+      if (_.isFunction(callback)) {
+        return callback(req, res);
+      } else {
+        return next('route');
+      }
+    },
+    paged: function(path) {
+      return function(req, res, next) {
+        var i, page, path_var, path_vars, redirection;
+        redirection = path + '';
+        path_vars = _.filter(path.split('/'), function(i) {
+          return i.charAt(0) === ':';
+        });
+        for (path_var in path_vars) {
+          i = path_vars[path_var];
+          redirection = redirection.replace(i, req.params[i.substring(1)]);
+        }
+        page = parseInt(req.param('page', 1));
+        if (page === 1) return res.redirect(redirection, 301);
+        return middleware.hmvc(req, res, next, path);
+      };
+    },
+    remove_trailing_slash: function(req, res, next) {
+      var url;
+      url = req.originalUrl;
+      if (url.length > 1 && url.charAt(url.length - 1) === '/') {
+        url = url.substring(0, url.length - 1);
+        return res.redirect(url, 301);
+      }
+      return next();
+    }
+  };
 
-app.get('/single/', function(req, res){
-  options.locals.body_class = 'single';
-  res.render('gallery_single', options);
-})
+  app.param('sort', function(req, res, next, id) {
+    if (id === 'ultimas' || id === 'top' || id === 'galeria') {
+      return next();
+    } else {
+      return next('route');
+    }
+  });
 
-app.get('/upload/', function(req, res){
-  options.locals.body_class = 'upload';
-  res.render('gallery_upload', options);
-})
+  app.param('user', function(req, res, next, id) {
+    if (id !== 'ultimas' && id !== 'top' && id !== 'galeria' && id !== 'pag') {
+      return next();
+    } else {
+      return next('route');
+    }
+  });
 
-app.all('*', function(req, res){
-  options.locals.body_class = 'gallery liquid';
-  res.render('gallery', options);
-});
+  app.param('page', function(req, res, next, id) {
+    if (id.match(/[0-9]+/)) {
+      req.param.page = parseInt(req.param.page);
+      return next();
+    } else {
+      return next(404);
+    }
+  });
 
-// Only listen on $ node app.js
-if (!module.parent) {
-  app.listen(10265);
-  console.log("Listening on port %d", app.address().port);
-}
+  app.all('*', middleware.remove_trailing_slash, function(req, res, next) {
+    res.locals({
+      _: underscore,
+      body_class: ''
+    });
+    return next('route');
+  });
+
+  app.get('/', function(req, res) {
+    res.local('body_class', 'home gallery');
+    return res.render('gallery');
+  });
+
+  app.get('/fotos/:user/:slug', function(req, res) {
+    res.local('body_class', 'single');
+    return res.render('gallery_single');
+  });
+
+  app.get('/fotos/:user/:slug/editar', function(req, res) {
+    var slug, user;
+    user = req.param('user');
+    slug = req.param('slug');
+    return res.send("PUT /fotos/" + user + "/" + slug + "/editar", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.put('/fotos/:user/:slug', function(req, res) {
+    var slug, user;
+    user = req.param('user');
+    slug = req.param('slug');
+    return res.send("PUT /fotos/" + user + "/" + slug, {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app["delete"]('/fotos/:user/:slug', function(req, res) {
+    var slug, user;
+    user = req.param('user');
+    slug = req.param('slug');
+    return res.send("DELETE /fotos/" + user + "/" + slug, {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/fotos/:user/:slug/sizes/:size', function(req, res) {
+    res.local('body_class', 'sizes');
+    return res.render('gallery_single_large');
+  });
+
+  app.get('/fotos/:user', function(req, res) {
+    res.local('body_class', 'gallery liquid');
+    return res.render('gallery');
+  });
+
+  app.get('/fotos/publicar', function(req, res) {
+    return res.render('gallery_upload');
+  });
+
+  app.post('/fotos/publicar', function(req, res) {
+    return res.send("POST /fotos/publicar", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/fotos/:sort/pag/:page?', middleware.paged('/fotos/:sort'));
+
+  app.get('/fotos/:sort', function(req, res, next) {
+    var page, sort;
+    sort = req.params.sort;
+    page = parseInt(req.param('page', 1));
+    return res.send("GET /fotos/" + sort + "/pag/" + page, {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/fotos/pag/:page?', middleware.paged('/fotos'));
+
+  app.get('/fotos', function(req, res) {
+    return res.send("GET /fotos", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/tags/:tag/pag/:page?', middleware.paged('/tags/:tag'));
+
+  app.get('/tags/:tag', function(req, res) {
+    var page, tag;
+    tag = req.params.tag;
+    page = parseInt(req.param('page', 1));
+    return res.send("GET /tags/" + tag + "/pag/" + page, {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/tags', function(req, res) {
+    return res.send("GET /fotos/tags", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/perfil', function(req, res) {
+    return res.send("GET /perfil", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.put('/perfil', function(req, res) {
+    return res.send("PUT /perfil", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/login', function(req, res) {
+    return res.send("GET /login", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.post('/login', function(req, res) {
+    return res.send("POST /login", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/registro', function(req, res) {
+    return res.send("GET /registro", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.post('/registro', function(req, res) {
+    return res.send("POST /registro", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  app.get('/logout', function(req, res) {
+    return res.send("GET /perfil", {
+      'Content-Type': 'text/plain'
+    });
+  });
+
+  if (!module.parent) {
+    app.listen(3000);
+    console.log("Listening on port %d", app.address().port);
+  }
+
+}).call(this);
